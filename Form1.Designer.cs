8bac5df0d215e2fb59e419e6d176d0a14f1a159c
Zheng <270490096@qq.com>
2018-02-05 11:01:28 +0800

    Signed-off-by: Zheng <270490096@qq.com>

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace 拼图
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.a = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.a)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(393, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(393, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "自动完成";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(393, 116);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "撤销";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(393, 145);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "重做";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(393, 200);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "关闭";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // a
+            // 
+            this.a.Location = new System.Drawing.Point(12, 12);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(300, 300);
+            this.a.TabIndex = 0;
+            this.a.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 321);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.a);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.a)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.PictureBox a;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 拼图
+{
+    public partial class Form1 : Form
+    {
+
+           PictureBox fBlankButton;
+        public Form1()
+        {
+            PictureBox[,] fButtoons;
+            
+            InitializeComponent();
+            Bitmap lImage = new Bitmap("Boy.jpg");
+            fButtoons = new PictureBox[4, 4];
+            for(int i=0;i<4;i++)
+                for (int j = 0; j < 4; j++)
+                {
+                    fButtoons[i, j] = new PictureBox();
+
+                    fButtoons[i, j].Size = new Size(75, 75);
+
+                    fButtoons[i, j].Location = new Point(i * 75, j * 75);
+
+                    fButtoons[i, j].BorderStyle = BorderStyle.FixedSingle;
+
+                    fButtoons[i, j].Image=lImage.Clone(fButtoons[i, j].Bounds, lImage.PixelFormat);
+             
+                    fButtoons[i, j].Click += new System.EventHandler(this.fButtoons_Click);
+                    //添加到拼版矩阵
+                    a.Controls.Add(fButtoons[i, j]);
+                }
+            fBlankButton=fButtoons[3,3];
+            fBlankButton.Visible = false;
+        }
+        private void fMoveButtonWithBlank(PictureBox aButton)
+        {
+            Point t = aButton.Location;
+            aButton.Location = fBlankButton.Location;
+            fBlankButton.Location = t;
+        }
+     private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void fButtoons_Click(object sender, EventArgs e)
+        {
+           PictureBox lCurrentButton = sender as PictureBox;
+           //判断一下lCurrentButton和fBlankButton的相邻关系
+          if (!((lCurrentButton.Top == fBlankButton.Top && Math.Abs(lCurrentButton.Left - fBlankButton.Left) == 75) || (lCurrentButton.Left == fBlankButton.Left && Math.Abs(lCurrentButton.Top - fBlankButton.Top) == 75)))
+               return;
+            fMoveButtonWithBlank(lCurrentButton);
+        }
+    }
+
+       
+    }
+
